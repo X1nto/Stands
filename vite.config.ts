590dfe5f90bdde86346d205cc1fcc393dfb76ad7
Xinto <burtkni@yandex.ru>
2023-01-07 02:00:06 +0400

    build for vercel in prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import solid from 'solid-start/vite';
 import solidSvg from 'vite-plugin-solid-svg';
+import node from 'solid-start-node';
+import vercel from 'solid-start-vercel';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [solid({ ssr: false }), solidSvg({ defaultAsComponent: true })],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      solid({ ssr: false, adapter: mode === 'production' ? vercel() : node() }),
+      solidSvg({ defaultAsComponent: true }),
+    ],
+  };
 });
